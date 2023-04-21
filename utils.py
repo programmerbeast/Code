7e4d903804eb78a5e98b58a8440b7306a36055ca
CJ-777 <64955484+CJ-777@users.noreply.github.com>
2023-04-21 12:06:31 +0530

    added review storage to csv and new functions to utils.

--- utils.py
+++ utils.py
@@ -1,4 +1,18 @@
-def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+import pickle
+from os import path
+import pandas as pd
+
+
+def printProgressBar(
+    iteration,
+    total,
+    prefix="",
+    suffix="",
+    decimals=1,
+    length=100,
+    fill="█",
+    printEnd="\r",
+):
     """
     Call in a loop to create terminal progress bar
     @params:
@@ -13,8 +27,32 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
     """
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filledLength = int(length * iteration // total)
-    bar = fill * filledLength + '-' * (length - filledLength)
-    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    bar = fill * filledLength + "-" * (length - filledLength)
+    print(f"\r{prefix} |{bar}| {percent}% {suffix}", end=printEnd)
     # Print New Line on Complete
-    if iteration == total: 
+    if iteration == total:
         print()
+
+
+def save_object(obj, filename):
+    with open(filename, "wb") as outp:  # Overwrites any existing file.
+        pickle.dump(obj, outp, pickle.HIGHEST_PROTOCOL)
+
+
+def get_object(filename):
+    obj = None
+    with open(filename, "rb") as inp:  # Overwrites any existing file.
+        obj = pickle.load(inp)
+        # print("Object not found.")
+    return obj
+
+
+def retrieve_app_id(appname):
+    file = path.join("Data", "name_id_map.csv")
+    df = pd.read_csv(file)
+    id = None
+    try:
+        id = df.loc[df["app_name"] == appname]["app_id"].values[0]
+    except:
+        print(f"{appname} not in map.")
+    return id
